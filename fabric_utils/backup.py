1b041c9db3e786c3d08bd4a9bcd6c39fb6a82763
Tomasz Wysocki <tomasz@wysocki.info>
2011-01-24 14:37:13 +0100

    Add TmpDir.

--- fabric_utils/backup.py
+++ fabric_utils/backup.py
@@ -5,3 +5,11 @@ def postgresql(database, file):
     sudo("chown postgres `dirname %s`" % file)
     sudo("pg_dump %s > %s" % (database, file), user='postgres')
     sudo("chown %s `dirname %s` %s" % (owner, file, file))
+
+class TmpDir(object):
+    def __enter__(self):
+        self.dir_name = run("mktemp -d")
+        return self.dir_name
+    
+    def __exit__(self, type, value, traceback):
+        sudo("rm -rf %s" % self.dir_name)
